39472b401dead9023c6c6ccd61fb93295527ae32
fschwiet <fschwiet@gmail.com>
2014-11-19 02:34:50 -0600

    added a close button to self-study

--- AnkiDroid/src/main/java/com/ichi2/anki/SelfStudyActivity.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/SelfStudyActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class SelfStudyActivity extends ActionBarActivity {
@@ -12,6 +14,14 @@ public class SelfStudyActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_self_study);
+
+        Button closeButton = (Button)findViewById(R.id.button_close_selfstudy);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SelfStudyActivity.this.finish();
+            }
+        });
     }
 
 
