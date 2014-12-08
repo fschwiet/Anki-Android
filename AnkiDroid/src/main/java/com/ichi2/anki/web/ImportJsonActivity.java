a55174f1c3d29853951a4c48098108812669f83b
fschwiet <fschwiet@gmail.com>
2014-12-07 18:46:27 -0600

    added a minimal activity to start importing json

---
+++ AnkiDroid/src/main/java/com/ichi2/anki/web/ImportJsonActivity.java
@@ -0,0 +1,18 @@
+package com.ichi2.anki.web;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.ichi2.anki.R;
+
+public class ImportJsonActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import_json);
+    }
+
+}
