edd04110cdbe42a67bb4a0a09a45f5ec67bb878e
fschwiet <fschwiet@gmail.com>
2014-12-08 15:09:23 -0600

    the import activity will take a file url as input and turn it into json for processing

--- AnkiDroid/src/main/java/com/ichi2/anki/web/ImportJsonActivity.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/web/ImportJsonActivity.java
@@ -1,18 +1,38 @@
 package com.ichi2.anki.web;
 
-import android.support.v7.app.ActionBarActivity;
+import android.annotation.TargetApi;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.util.JsonReader;
 
 import com.ichi2.anki.R;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+@TargetApi(11)
 public class ImportJsonActivity extends ActionBarActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) throws RuntimeException {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import_json);
+
+        try{
+            String importUrlAddress = getIntent().getExtras().getString("importUrl");
+            URL importUrl = new URL(importUrlAddress);
+            InputStream importStream = importUrl.openStream();
+
+
+            JsonReader reader = new JsonReader(new InputStreamReader(importStream));
+            reader.setLenient(true);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
 }
